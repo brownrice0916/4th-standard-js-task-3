27986059713f4cc7a78c79834f4b4f3591168d65
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-01-26 17:37:34 +0900

    d

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -5,8 +5,10 @@ function StudentList({ filteredStudents }) {
   console.log(filteredStudents);
   return (
     <ul>
-      {filteredStudents.map((student) => (
-        <li>{`${student.name}(${student.age}세) - ${student.grade}등급`}</li>
+      {filteredStudents.map((student, idx) => (
+        <li
+          key={idx}
+        >{`${student.name}(${student.age}세) - ${student.grade}등급`}</li>
       ))}
       {/* 여기에 학생 목록을 표시하는 로직을 작성하세요. */}
       {/* ex: 홍길동 (20세) - A등급 */}
